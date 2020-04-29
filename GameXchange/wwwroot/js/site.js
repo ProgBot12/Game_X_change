ec414c6faca12e7e642d1e960c9268013818ba30
ProgBot12 <emir.compwiz@gmail.com>
2020-04-28 21:05:44 -0400

    added a simple image slider
    removed the code from the previous homepage and added a new simple automatic slider

--- GameXchange/wwwroot/js/site.js
+++ GameXchange/wwwroot/js/site.js
@@ -1,31 +1,10 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$("#slideshow > div:gt(0)").hide();
 
-// Write your Javascript code.
-
-
-
-// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-(function () {
-
-    var slideContainer = $('.slide-container');
-
-    slideContainer.slick();
-
-    $('.clash-card__image img').hide();
-    $('.slick-active').find('.clash-card img').fadeIn(200);
-
-    // On before slide change
-    slideContainer.on('beforeChange', function (event, slick, currentSlide, nextSlide) {
-        $('.slick-active').find('.clash-card img').fadeOut(1000);
-    });
-
-    // On after slide change
-    slideContainer.on('afterChange', function (event, slick, currentSlide) {
-        $('.slick-active').find('.clash-card img').fadeIn(200);
-    });
-
-})();
+setInterval(function () {
+    $('#slideshow > div:first')
+        .fadeOut(1)
+        .next()
+        .fadeIn(1)
+        .end()
+        .appendTo('#slideshow');
+}, 4000);
